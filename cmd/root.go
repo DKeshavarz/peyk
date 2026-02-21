015f0a400548bc00ce7667cef60b373fa14f144c
DKeshavarz <dankeshavarz1075@gmail.com>
2026-02-21 03:58:11 +0330

    chore: add cmd entry point

---
+++ cmd/root.go
@@ -0,0 +1,33 @@
+/*
+Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+	Use:   "peyk",
+	Short: "A platform that connects different messaging apps together",
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {}
+
+
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2026 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/DKeshavarz/peyk/cmd"
+
+func main() {
+	cmd.Execute()
+}
